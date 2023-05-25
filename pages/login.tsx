f65175e0b5ca59080e36cc16a5383ccce9ab1d64
Argam <argam@ucraft.com>
2023-05-25 14:44:47 +0400

    c11

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,21 +2,23 @@ import React, { useEffect } from 'react'
 
 const Test = () => {
   useEffect(() => {
-    (function (d) {
-      let js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-      if (d.getElementById(id)) { return; }
-      js = d.createElement('script'); js.id = id; js.async = true;
+    const id = 'facebook-jssdk';
+    const ref = document.getElementsByTagName('script')[0];
+    if (!document.getElementById(id)) {
+      let js = document.createElement('script');
+      js.id = id;
+      js.async = true;
       js.src = "//connect.facebook.net/en_US/all.js";
-      ref.parentNode.insertBefore(js, ref);
-    }(document));
-
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        appId: '766633211776568',
-        cookie: true,
-        xfbml: true,
-        version: 'v17.0'
-      });
+      ref.parentNode && ref.parentNode.insertBefore(js, ref);
+  
+      window.fbAsyncInit = function () {
+        window.FB.init({
+          appId: '766633211776568',
+          cookie: true,
+          xfbml: true,
+          version: 'v17.0'
+        });
+      }
     }
   }, []);
 
@@ -47,12 +49,14 @@ const Test = () => {
     form.setAttribute('action', oauth2Endpoint);
 
     // Parameters to pass to OAuth 2.0 endpoint.
-    var params = {'client_id': '351171074715-38gr42ulu7b22mkpf1ildmbs41d285hl',
-                  'redirect_uri': 'https://nextjs-app2-seven.vercel.app',
-                  'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
-                  'state': 'try_sample_request',
-                  'include_granted_scopes': 'true',
-                  'response_type': 'token'};
+    var params = {
+      'client_id': '351171074715-38gr42ulu7b22mkpf1ildmbs41d285hl',
+      'redirect_uri': 'https://nextjs-app2-seven.vercel.app',
+      'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+      'state': 'try_sample_request',
+      'include_granted_scopes': 'true',
+      'response_type': 'token'
+    };
 
     // Add form parameters as hidden input values.
     for (var p in params) {
