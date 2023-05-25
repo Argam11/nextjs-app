4865b944006e1156b19532a5592792600f591966
Argam <argam@ucraft.com>
2023-05-25 11:57:37 +0400

    c4

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,68 +1,83 @@
 import React, { useEffect } from 'react'
 
 const Test = () => {
-    useEffect(() => {
-        if(window) {
-            console.log('window.FB', window.FB)
-            window.FB.init({
-                appId: '766633211776568',
-                cookie: true,                     // Enable cookies to allow the server to access the session.
-                xfbml: true,                     // Parse social plugins on this webpage.
-                version: 'v17.0'           // Use this Graph API version for this call.
-            });
-      
-            window.FB.getLoginStatus(function (response: any) {   // Called after the JS SDK has been initialized.
-              statusChangeCallback(response);        // Returns the login status.
-            //   console.log('response', response);
-            });
-         console.log('window.fbAsyncInit', window.fbAsyncInit)
-        }
-      }, []);
+  useEffect(() => {
+    // (function (d) {
+    //   let js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+    //   if (d.getElementById(id)) { return; }
+    //   js = d.createElement('script'); js.id = id; js.async = true;
+    //   js.src = "//connect.facebook.net/en_US/all.js";
+    //   ref.parentNode.insertBefore(js, ref);
+    // }(document));
 
-      function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-        console.log('statusChangeCallback');
-        console.log(response, 11);                   // The current login status of the person.
-        if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-          testAPI();  
+    // window.fbAsyncInit = function () {
+    //   console.log('window.FB 11', window.FB);
+    //   window.FB.init({
+    //     appId: '766633211776568',
+    //     cookie: true,
+    //     xfbml: true,
+    //     version: 'v17.0'
+    //   });
+
+    //   window.FB.getLoginStatus(function (response: any) {
+    //     statusChangeCallback(response);
+    //   });
+    // }
+  }, []);
+
+  function statusChangeCallback(response: any) {
+    console.log(response, 11);
+    if (response.status === 'connected') {
+      console.log('You are connected');
+    } else {
+      console.log('Please login to this webpage.');
+      window.FB.login((res: any) => {
+        console.log('res', res)
+      })
+    }
+  }
+
+  const onFacebookLogin = () => {
+    (function (d) {
+      let js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement('script'); js.id = id; js.async = true;
+      js.src = "//connect.facebook.net/en_US/all.js";
+      ref.parentNode.insertBefore(js, ref);
+    }(document));
+
+    window.fbAsyncInit = function () {
+      console.log('window.FB 11', window.FB);
+      window.FB.init({
+        appId: '766633211776568',
+        cookie: true,
+        xfbml: true,
+        version: 'v17.0'
+      });
+
+      window.FB.getLoginStatus(function (response: any) {
+        if (response.status === 'connected') {
+          console.log('You are connected');
         } else {
-            console.log('Please login to this webpage.');
-            window.FB.login(res => {
-                console.log('res', res)
-            })
+          console.log('Please login to this webpage.');
+          window.FB.login((res: any) => {
+            console.log('res', res)
+          })
         }
-      }
-      function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-        console.log('Welcome!  Fetching your information.... ');
-        window.FB.api('/me', function(response) {
-          console.log('Successful login for: ', response);
-        });
-      }
-    
-      const onFacebookLogin = () => {
-        if(window) {
-          console.log(123);
-      
-          FB.getLoginStatus(function(response) {   // See the onlogin handler
-            statusChangeCallback(response);
-          });
-        }
-      };
-      const onFacebookLogout = () => {
-        if(window) {
-          console.log(123);
-      
-          FB.getLoginStatus(function(response) {   // See the onlogin handler
-            statusChangeCallback(response);
-          });
-        }
-      };
+      });
+    }
+  };
+
+  const onGoogleLogin = () => {
+    window.FB.getLoginStatus(function (response: any) {
+      statusChangeCallback(response);
+    });
+  };
 
   return (
     <div>
-
-
-        <button onClick={onFacebookLogin}>Login with Facebook 2</button>
-        <button onClick={onFacebookLogout}>Logout with Facebook</button>
+      <button onClick={onGoogleLogin}>Login with Google</button>
+      <button onClick={onFacebookLogin}>Login with Facebook</button>
     </div>
   )
 }
