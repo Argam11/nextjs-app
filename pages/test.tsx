530e591c3062ab05a449186eb776a3f0bde512ed
Argam <argam@ucraft.com>
2023-05-25 12:01:30 +0400

    c5

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,42 +2,6 @@ import React, { useEffect } from 'react'
 
 const Test = () => {
   useEffect(() => {
-    // (function (d) {
-    //   let js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-    //   if (d.getElementById(id)) { return; }
-    //   js = d.createElement('script'); js.id = id; js.async = true;
-    //   js.src = "//connect.facebook.net/en_US/all.js";
-    //   ref.parentNode.insertBefore(js, ref);
-    // }(document));
-
-    // window.fbAsyncInit = function () {
-    //   console.log('window.FB 11', window.FB);
-    //   window.FB.init({
-    //     appId: '766633211776568',
-    //     cookie: true,
-    //     xfbml: true,
-    //     version: 'v17.0'
-    //   });
-
-    //   window.FB.getLoginStatus(function (response: any) {
-    //     statusChangeCallback(response);
-    //   });
-    // }
-  }, []);
-
-  function statusChangeCallback(response: any) {
-    console.log(response, 11);
-    if (response.status === 'connected') {
-      console.log('You are connected');
-    } else {
-      console.log('Please login to this webpage.');
-      window.FB.login((res: any) => {
-        console.log('res', res)
-      })
-    }
-  }
-
-  const onFacebookLogin = () => {
     (function (d) {
       let js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
       if (d.getElementById(id)) { return; }
@@ -54,18 +18,32 @@ const Test = () => {
         xfbml: true,
         version: 'v17.0'
       });
-
-      window.FB.getLoginStatus(function (response: any) {
-        if (response.status === 'connected') {
-          console.log('You are connected');
-        } else {
-          console.log('Please login to this webpage.');
-          window.FB.login((res: any) => {
-            console.log('res', res)
-          })
-        }
-      });
     }
+  }, []);
+
+  function statusChangeCallback(response: any) {
+    console.log(response, 11);
+    if (response.status === 'connected') {
+      console.log('You are connected');
+    } else {
+      console.log('Please login to this webpage.');
+      window.FB.login((res: any) => {
+        console.log('res', res)
+      })
+    }
+  }
+
+  const onFacebookLogin = () => {
+    window.FB.getLoginStatus(function (response: any) {
+      if (response.status === 'connected') {
+        console.log('You are connected');
+      } else {
+        console.log('Please login to this webpage.');
+        window.FB.login((res: any) => {
+          console.log('res', res)
+        });
+      }
+    });
   };
 
   const onGoogleLogin = () => {
