b2c3773caed9fa6413d0033c31570a8cd22d14e2
Argam11 <45657855+Argam11@users.noreply.github.com>
2023-06-06 19:17:17 +0000

    C5

--- pages/login.tsx
+++ pages/login.tsx
@@ -45,12 +45,12 @@ const Test = () => {
   useEffect(() => {
     if(window.google && googleLoad) {
       window.google.accounts.id.initialize({
-        auto_select: false,
+        // auto_select: false,
         client_id: '351171074715-38gr42ulu7b22mkpf1ildmbs41d285hl',
         callback: (res: any) => {
           console.log(11, res);
         },
-        ux_mode: 'popup',
+        // ux_mode: 'popup',
         // login_uri: `${baseURL}/callback/auth/google`
       });
     }
