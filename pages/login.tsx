3f5408d7df12a27df5ebd79d0a45020e9950a592
Argam11 <45657855+Argam11@users.noreply.github.com>
2023-06-21 13:52:15 +0000

    C3

--- pages/login.tsx
+++ pages/login.tsx
@@ -81,47 +81,78 @@ const Test = () => {
     window.FB.logout();
   };
 
-  useEffect(() => {
-    if(window.google && googleLoad) {
-      window.google.accounts.id.initialize({
-        client_id: '351171074715-38gr42ulu7b22mkpf1ildmbs41d285hl.apps.googleusercontent.com',
-        ux_mode: "redirect",
-        // callback: (res: any) => {
-        //   console.log('res', res);
-        //   console.log(11, jwt_decode(res.credential));
+  // useEffect(() => {
+  //   if(window.google && googleLoad) {
+  //     window.google.accounts.id.initialize({
+  //       client_id: '351171074715-38gr42ulu7b22mkpf1ildmbs41d285hl.apps.googleusercontent.com',
+  //       ux_mode: "redirect",
+  //       // callback: (res: any) => {
+  //       //   console.log('res', res);
+  //       //   console.log(11, jwt_decode(res.credential));
 
-        // },
-        // ux_mode: 'popup',
-        login_uri: `https://nextjs-app-beryl-seven.vercel.app/login`
-      });
+  //       // },
+  //       // ux_mode: 'popup',
+  //       // login_uri: `https://nextjs-app-beryl-seven.vercel.app/login`
+  //     });
 
-      if (googleButtonRef.current) {
-        window.google.accounts.id.renderButton(googleButtonRef.current, {
-          type: 'standard',
-          theme: 'outline',
-          size: 'large'
-        });
-      }
-    }
-  }, [googleLoad])
-
-  // const onGoogleLogin = () => {
-  //   console.log(window.google, 22);
-    
-  //   if (window.google) {
-  //     // Cookies.remove('g_state');
-  //     // window.google.accounts.id.prompt();
-
-  //     // const googleButtonWrapper = createFakeGoogleWrapper();
-  //     // googleButtonWrapper.click();
+  //     if (googleButtonRef.current) {
+  //       window.google.accounts.id.renderButton(googleButtonRef.current, {
+  //         type: 'standard',
+  //         theme: 'outline',
+  //         size: 'large'
+  //       });
+  //     }
   //   }
-  // };
+  // }, [googleLoad])
+
+  const onGoogleLogin = () => {
+    // console.log(window.google, 22);
+    
+    // if (window.google) {
+      // Cookies.remove('g_state');
+      // window.google.accounts.id.prompt();
+
+      // const googleButtonWrapper = createFakeGoogleWrapper();
+      // googleButtonWrapper.click();
+    // }
+
+
+
+      // Google's OAuth 2.0 endpoint for requesting an access token
+      var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+    
+      // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+      var form = document.createElement('form');
+      form.setAttribute('method', 'GET'); // Send as a GET request.
+      form.setAttribute('action', oauth2Endpoint);
+    
+      // Parameters to pass to OAuth 2.0 endpoint.
+      var params = {'client_id': '351171074715-38gr42ulu7b22mkpf1ildmbs41d285hl.apps.googleusercontent.com',
+                    'redirect_uri': window.location.href,
+                    'response_type': 'token',
+                    'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+                    'include_granted_scopes': 'true',
+                    'state': 'pass-through value'};
+    
+      // Add form parameters as hidden input values.
+      for (var p in params) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'hidden');
+        input.setAttribute('name', p);
+        input.setAttribute('value', params[p]);
+        form.appendChild(input);
+      }
+    
+      // Add form to page and submit it to open the OAuth 2.0 endpoint.
+      document.body.appendChild(form);
+      form.submit();
+  };
 
   return (
     <div>
       {/* <Script src='//connect.facebook.net/en_US/all.js' /> */}
       <Script src='https://accounts.google.com/gsi/client' onReady={() => setGoogleLoad(true)} />
-      {/* <button onClick={onGoogleLogin}>Login with Google</button> */}
+      <button onClick={onGoogleLogin}>Login with Google</button>
       <button onClick={onFacebookLogin}>Login with Facebook</button>
       <button onClick={onFacebookLogout}>Logout Facebook</button>
       <div ref={googleButtonRef}></div>
