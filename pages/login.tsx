2be050ab0991c090d0d48cca9a1adc4ebf4e5e72
Argam11 <45657855+Argam11@users.noreply.github.com>
2023-07-27 12:43:59 +0000

    C3

--- pages/login.tsx
+++ pages/login.tsx
@@ -100,7 +100,7 @@ const Test = () => {
           type: 'standard',
           theme: 'outline',
           size: 'large',
-          width: '360px'
+          width: '260px'
         });
       }
     }
