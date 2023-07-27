1fdb96934a127fb7a19b97dba1d18d564505b4b0
Argam11 <45657855+Argam11@users.noreply.github.com>
2023-07-27 12:49:10 +0000

    C6

--- pages/login.tsx
+++ pages/login.tsx
@@ -100,7 +100,7 @@ const Test = () => {
           type: 'standard',
           theme: 'outline',
           size: 'large',
-          width: 360
+          width: '360px'
         });
       }
     }
